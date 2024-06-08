79a10f93719683c01933d01ecc63060ff2d462a3
TooManyChoices <81335262+TooManyChoices@users.noreply.github.com>
2024-06-07 19:22:22 -0500

    finally changed the intents

--- Program.cs
+++ Program.cs
@@ -22,7 +22,7 @@ namespace Bot
             DiscordSocketConfig socketconfig = new DiscordSocketConfig
             {
                 LogLevel = (LogSeverity)(5 - Config.GetSetting<int>("ignore-log-severity", 0)),
-                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.GuildMembers
+                GatewayIntents = GatewayIntents.GuildMembers | GatewayIntents.MessageContent
             };
             _client = new DiscordSocketClient(socketconfig);
             _client.Log += Log;
